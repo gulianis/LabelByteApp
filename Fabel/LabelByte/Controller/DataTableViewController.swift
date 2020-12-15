44252fd5fdf7cdf9e4eb71c750306719d73b4d28
gulianis <gulianisach@gmail.com>
2020-12-15 06:45:08 -0800

    Changes

---
+++ Fabel/LabelByte/Controller/DataTableViewController.swift
@@ -0,0 +1,218 @@
+//
+//  ImageTableViewController.swift
+//  LabelByte
+//
+//  Created by Sachin Guliani on 8/4/20.
+//
+
+import UIKit
+import CoreData
+
+class DataTableView: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    var account: Account?
+    
+    var zipFileObject: ZipFile?
+    
+    var fetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>!
+    
+    func fetchCall() {
+        // Load Image Names for current account
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Data")
+        let sortByTitle = NSSortDescriptor(key: "saved", ascending: true)
+        let sortByName = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortByTitle, sortByName]
+        let predicate = NSPredicate(format: "ofZipFile == %@", zipFileObject!)
+        //let predicate = NSPredicate(format: "ofZipFile.name == %@", zipName!)
+        fetchRequest.predicate = predicate
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: "saved", cacheName: nil)
+        fetchedResultsController.delegate = self
+        do {
+            try fetchedResultsController.performFetch()
+            tableView.reloadData()
+        } catch {
+            fatalError("Failed to fetch entities: \(error)")
+        }
+    }
+
+    func recieveImageNameDict(_ name: String) -> [String: String] {
+        // Gets Image Names from server
+        var DictString = ""
+        // Uses semaphore to wait for network call till data recieved
+        let semaphore = DispatchSemaphore(value: 0)
+        RecieveImageNames(name) { output in
+            DictString = output
+            semaphore.signal()
+        }
+        semaphore.wait()
+        // Converts DictString to usable format
+        let jsonData = DictString.data(using: .utf8)!
+        let dictionary = try? JSONSerialization.jsonObject(with: jsonData)
+        if let dataDictionary = dictionary as? [String: String] {
+            return dataDictionary
+        }
+        return [:]
+        
+    }
+    
+    func imageNames() {
+        // Saves image names to Core Data
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Data")
+        let dataDictionary = recieveImageNameDict(zipFileObject!.name!)
+        for (imageName, saved) in dataDictionary {
+            let image = Data(context: context)
+            image.name = imageName
+            if saved == "false" {
+                image.saved = false
+                zipFileObject?.unsaved_data_count += 1
+            } else {
+                image.saved = true
+            }
+            image.clicked_count = 0
+            zipFileObject!.addToFile(image)
+            //zipFileObject!.unsaved_data_count += 1
+            save()
+        }
+    }
+    
+    func CallImageRequestCount(_ imageName: String) -> Int {
+        var result = ""
+        // Gets Image Count from server
+        let semaphore = DispatchSemaphore(value: 0)
+        // Uses semaphore to wait for network call till data recieved
+        ImageRequestCount(zipFileObject!.name!, imageName) { output in
+            result = output
+            semaphore.signal()
+        }
+        semaphore.wait()
+        // Converts DictString to usable format
+        let jsonData = result.data(using: .utf8)!
+        let dictionary = try? JSONSerialization.jsonObject(with: jsonData)
+        if let Data = dictionary as? [String: Int] {
+            return Data["result"]!
+        }
+        return 0
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.hidesBackButton = false
+        self.navigationItem.title = "Images in Zip Files"
+        
+
+        //self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        // Only Call if never been accessed before
+        // Images in a zip file can never change
+        tableView.tableFooterView = UIView()
+        //imageNames()
+        if zipFileObject!.clicked == false {
+            imageNames()
+        }
+        zipFileObject!.clicked = true
+        save()
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchCall()
+        clearCache()
+    }
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        if let frc = fetchedResultsController {
+            return frc.sections!.count
+        }
+        return 0
+    }
+    
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        // Categorizes into has been labeled or not
+        switch (zipFileObject?.completed) {
+        case true:
+            return "Labeled"
+        default:
+            switch (section) {
+            case 0:
+                return "UnLabeled"
+            default:
+                return "Labeled"
+            }
+        }
+    }
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        guard let sections = self.fetchedResultsController?.sections else {
+            fatalError("No sections in fetchedResultsController")
+        }
+        let sectionInfo = sections[section]
+        return sectionInfo.numberOfObjects
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cellId") else {
+                return UITableViewCell(style: .value1, reuseIdentifier: "cellId")
+                    }
+                    return cell
+                }()
+        // At this point, we definitely have a cell -- either dequeued or newly created,
+        // so let's force unwrap the optional into a UITableViewCell
+        guard let object = self.fetchedResultsController?.object(at: indexPath) else {
+            fatalError("Attempt to configure cell without a managed object")
+        }
+        let image = object as! Data
+        cell.textLabel?.text = image.name
+        cell.detailTextLabel?.text = String(10 - image.clicked_count)
+        return cell
+
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let VC = ImageViewController()
+        guard let object = self.fetchedResultsController?.object(at: indexPath) else {
+            fatalError("Attempt to configure cell without a managed object")
+        }
+        // Check Core Data to see if image has been accessed less than 10 times
+        // Mechanism to limit server requests
+        let image = object as! Data
+        if image.clicked_count < 10 {
+            // Update image count with server count
+            // Only push new view if image has been accessed less than 10 times
+            let count = Int(CallImageRequestCount(image.name!))
+            //let count = 7
+            image.clicked_count = Int16(count)
+            save()
+            if image.clicked_count < 10 {
+                image.clicked_count += 1
+                save()
+                VC.imageData = image
+                self.navigationController?.pushViewController(VC, animated: true)
+            } else {
+                showLimitView()
+            }
+        } else {
+            showLimitView()
+        }
+    }
+    
+    func showLimitView() {
+        self.definesPresentationContext = true
+        let vc = SuccessViewController()
+        vc.modalPresentationStyle = .overCurrentContext
+        vc.modalTransitionStyle = .crossDissolve
+        vc.message = "Reached Access Limit"
+        present(vc, animated: true, completion: nil)
+        tableView.reloadData()
+    }
+
+}
+
---
+++ Fabel/LabelByte/Controller/LoginViewController.swift
@@ -0,0 +1,168 @@
+//
+//  LoginViewController.swift
+//  LabelByte
+//
+//  Created by Sachin Guliani on 6/19/20.
+//
+
+import UIKit
+import CoreData
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.LoginButton.addTarget(self, action: #selector(ButtonPress), for: .touchUpInside)
+        self.LoginButton.addTarget(self, action: #selector(DuringButtonPress), for: .touchDown)
+        // Detect Keyboard
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+        
+    override func viewWillAppear(_ animated: Bool) {
+        // Make sure screen goes to normal before appearing
+        // if keyboard still occuring in login screen
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+        
+        let largest = max(self.view.frame.width, self.view.frame.height)
+        if largest < 600 {
+            TotalStackView.spacing = 35
+            LoginLabelInfo.font = UIFont.systemFont(ofSize: 15.0)
+            LinkLabelInfo.font = UIFont.systemFont(ofSize: 13.0)
+            TitleLabelInfo.font = UIFont(name: "Gill Sans", size: 50)
+        }
+        /*
+        if self.view.frame.width < self.view.frame.height {
+            print("HERe")
+            if self.view.frame.height < 600 {
+                TotalStackView.spacing = 35
+                LoginLabelInfo.font = UIFont.systemFont(ofSize: 15.0)
+                LinkLabelInfo.font = UIFont.systemFont(ofSize: 13.0)
+                TitleLabelInfo.font = UIFont(name: "Gill Sans", size: 50)
+            }
+        } else {
+            print("THERE")
+            print(self.view.frame.height)
+            if self.view.frame.height < 330 {
+                TotalStackView.spacing = 35
+                LoginLabelInfo.font = UIFont.systemFont(ofSize: 15.0)
+                LinkLabelInfo.font = UIFont.systemFont(ofSize: 13.0)
+                TitleLabelInfo.font = UIFont(name: "Gill Sans", size: 50)
+            }
+        }
+        */
+    }
+        
+    override func loadView() {
+        let view = LoginView()
+        view.backgroundColor = .white
+            
+        let tap = UITapGestureRecognizer(target: self, action: #selector(Tapped))
+        tap.numberOfTapsRequired = 1
+        view.addGestureRecognizer(tap)
+
+        self.view = view
+    }
+        
+    unowned var MainView: LoginView { return self.view as! LoginView }
+    unowned var Username: UITextField { return MainView.EmailInfo }
+    unowned var Password: UITextField { return MainView.PasswordInfo }
+    unowned var LoginButton: UIButton { return MainView.LoginButtonInfo }
+    unowned var TotalStackView: UIStackView { return MainView.TotalStackView }
+    unowned var LoginLabelInfo: UILabel { return MainView.LoginLabelInfo }
+    unowned var LinkLabelInfo: UILabel { return MainView.LinkLabelInfo }
+    unowned var TitleLabelInfo: UILabel { return MainView.TitleLabelInfo }
+
+    func GetToken(_ username: String, _ password: String) -> String {
+        // Gets Token from server
+        var DictString = ""
+        var tokenResult = "error"
+        // Uses semaphore to wait for network call till data recieved
+        let semaphore = DispatchSemaphore(value: 0)
+        ReceiveToken(Username.text!, Password.text!) { output in
+            DictString = output
+            semaphore.signal()
+        }
+        semaphore.wait()
+        // Converts and parses data
+        let jsonData = DictString.data(using: .utf8)!
+        let dictionary = try? JSONSerialization.jsonObject(with: jsonData)
+        if let tokenDictionary = dictionary as? [String: Any] {
+            if let token = tokenDictionary["token"] as? String {
+                tokenResult = token
+            }
+        }
+        return tokenResult
+    }
+        
+    @objc func Tapped() {
+        // Get out of textbox
+        Username.resignFirstResponder()
+        Password.resignFirstResponder()
+    }
+            
+    @objc func ButtonPress() {
+        Username.resignFirstResponder()
+        Password.resignFirstResponder()
+        let token = GetToken(Username.text!, Password.text!)
+        // Get token from server
+        if token != "error" {
+            // Save Token using KeyChain to Encrypt
+            let data = KeyChain.convertStrToNSData(string: token)
+            KeyChain.save(key: Username.text!, data: data)
+            currentUsername = Username.text!
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Account")
+            let predicate = NSPredicate(format: "username = %@", Username.text!)
+            request.predicate = predicate
+            do {
+                let results = try context.fetch(request)
+                let VC = FileTableViewController()
+                // Only save if account has not been used before
+                switch (results.count) {
+                    case 0:
+                        let account = Account(context: context)
+                        account.username = Username.text!
+                        VC.account = account
+                        save()
+                    default:
+                        VC.account = (results[0] as! Account)
+                }
+                Username.text = ""
+                Password.text = ""
+                self.view.frame.origin.y = 0
+                self.navigationController?.pushViewController(VC, animated: true)
+            }
+            catch {
+                print("Failed")
+            }
+        }
+        LoginButton.backgroundColor = .systemBlue
+    }
+        
+    @objc func DuringButtonPress() {
+        LoginButton.backgroundColor = .gray
+    }
+        
+    @objc func keyboardWillShow(notification: NSNotification) {
+        // Push up textbox when keyboard shows up
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= 3*(keyboardSize.height) / 4
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        // Put textbox in original position when keyboard gone
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+}
+
+    
+
+
